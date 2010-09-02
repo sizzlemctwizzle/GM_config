sizzlemctwizzle/GM_config

commit c0136bf0805fde0fa740518b14c53465c65a74e5

Store values temporarily after they have been read to make saving easier.

gm_config.js
@@ -1,5 +1,5 @@
 // @name              GM_config
-// @version           1.4.2
+// @version           1.4.3
 // @contributors      JoeSimmons & SizzleMcTwizzle & IzzySoft & MartiMartz
 
 // The GM_config constructor
@@ -206,9 +206,16 @@ GM_configStruct.prototype = {
 
   save: function () {
     var fields = this.fields;
-    for (id in fields)
-      if (!fields[id].toValue())
-        return; // invalid value encountered
+    for (id in fields) {
+      var field = fields[id],
+          val = field.toValue();
+
+      if (val === null) { // invalid value encountered
+        this.readVals = {};
+        return;
+      } else if (field.settings.type != "button")
+        this.readVals[id] = val;
+    }
 
     this.write();
 
@@ -243,22 +250,13 @@ GM_configStruct.prototype = {
     return this.fields[name].value;
   },
 
-  log: (this.isGM) ? GM_log : ((window.opera) ? opera.postError : console.log),
-
   write: function (store, obj) {
-    // Build a list of values to save
-    if (!obj) {
-      var values = {},
-          fields = this.fields;
-      for (var id in fields)
-        values[id] = fields[id].value;
-    }
-
     try {
-      this.setValue(store || this.id, this.stringify(obj || values));
+      this.setValue(store || this.id, this.stringify(obj || this.readVals));
     } catch(e) {
       this.log("GM_config failed to save settings!");
     }
+    this.readVals = {};
   },
 
   read: function (store) {
@@ -353,6 +351,7 @@ GM_configStruct.prototype = {
   onClose: null,
   id: 'GM_config',
   fields: {},
+  readVals: {},
   title: 'User Script Settings',
   css: {
     basic:     "#GM_config * { font-family: arial,tahoma,myriad pro,sans-serif; }"
@@ -414,6 +413,7 @@ GM_configStruct.prototype = {
   GM_configStruct.prototype.getValue = getValue;
   GM_configStruct.prototype.stringify = stringify;
   GM_configStruct.prototype.parser = parser;
+  GM_configStruct.prototype.log = isGM ? GM_log : (window.opera ? opera.postError : console.log);
 })();
 
 function GM_configDefaultValue(type) {
@@ -574,7 +574,8 @@ GM_configField.prototype = {
   toValue: function() {
     var node = this.node,
         field = this.settings,
-        type = field.type;
+        type = field.type,
+        rval;
 
     switch (type) {
       case 'checkbox':
@@ -595,7 +596,7 @@ GM_configField.prototype = {
         var num = Number(node.value);
         if (isNaN(num) || Math.ceil(num) != Math.floor(num)) {
           alert('Field labeled "' + field.label + '" expects an integer value.');
-          return false;
+          return null;
         }
         this.value = num;
         break;
@@ -603,7 +604,7 @@ GM_configField.prototype = {
         var num = Number(node.value);
         if (isNaN(num)) {
           alert('Field labeled "' + field.label + '" expects a number value.');
-          return false;
+          return null;
         }
         this.value = num;
         break;
@@ -612,7 +613,7 @@ GM_configField.prototype = {
         break;
     }
 
-    return true; // value read successfully
+    return this.value; // value read successfully
   }
 };
 

commit 7064fbe963061eb1843863579ec7476eea859b8a

Bug fix. Reverting save and write functions to 0ab0096a21d324e8695e4c234477a0030cdd9fcb to kill a bug that was introduced in c0136bf0805fde0fa740518b14c53465c65a74e5.

gm_config.js
@@ -206,16 +206,9 @@ GM_configStruct.prototype = {
 
   save: function () {
     var fields = this.fields;
-    for (id in fields) {
-      var field = fields[id],
-          val = field.toValue();
-
-      if (val === null) { // invalid value encountered
-        this.readVals = {};
+    for (id in fields)
+      if (fields[id].toValue() === null) // invalid value encountered
         return;
-      } else if (field.settings.type != "button")
-        this.readVals[id] = val;
-    }
 
     this.write();
 
@@ -235,7 +228,7 @@ GM_configStruct.prototype = {
 
     // Null out all the fields so we don't leak memory
     var fields = this.fields;
-    for (id in fields)
+    for (var id in fields)
       fields[id].node = null;
 
     if (this.onClose) 
@@ -251,12 +244,21 @@ GM_configStruct.prototype = {
   },
 
   write: function (store, obj) {
+    if (!obj) {
+      var readVals = {},
+          fields = this.fields;
+
+      for (var id in fields) {
+        var field = fields[id];
+        if (field.settings.type != "button")
+          readVals[id] = field.value;
+      }
+    }
     try {
-      this.setValue(store || this.id, this.stringify(obj || this.readVals));
+      this.setValue(store || this.id, this.stringify(obj || readVals));
     } catch(e) {
       this.log("GM_config failed to save settings!");
     }
-    this.readVals = {};
   },
 
   read: function (store) {
@@ -351,7 +353,6 @@ GM_configStruct.prototype = {
   onClose: null,
   id: 'GM_config',
   fields: {},
-  readVals: {},
   title: 'User Script Settings',
   css: {
     basic:     "#GM_config * { font-family: arial,tahoma,myriad pro,sans-serif; }"