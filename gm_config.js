050978c9d41245b135404a3692ac42418a09be24
sizzlemctwizzle <medleymind@gmail.com>
2010-10-28 18:34:26 -0500

    Releasing GM_config under the LGPL. closes #28

--- gm_config.js
+++ gm_config.js
@@ -1,6 +1,28 @@
-// @name              GM_config
-// @version           1.4.5
-// @contributors      JoeSimmons & SizzleMcTwizzle & IzzySoft & MartiMartz
+/*
+Copyright 2009-2010, GM_config Contributors
+All rights reserved.
+
+GM_config Contributors:
+    Mike Medley <medleymind@gmail.com>
+    Joe Simmons
+    Izzy Soft
+    Marti Martz
+
+GM_config is distributed under the terms of the GNU Lesser General Public License.
+
+    GM_config is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 // The GM_config constructor
 function GM_configStruct() {
