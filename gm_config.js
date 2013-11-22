5ab3f74b64be5818dccb2ea87f07f6bff386b6b5
sizzlemctwizzle <medleymind@gmail.com>
2013-11-22 15:55:32 -0600

    Rewire the logic for choosing what to use for logging. Closes #59.

--- gm_config.js
+++ gm_config.js
@@ -478,7 +478,11 @@ GM_configStruct.prototype = {
   GM_configStruct.prototype.getValue = getValue;
   GM_configStruct.prototype.stringify = stringify;
   GM_configStruct.prototype.parser = parser;
-  GM_configStruct.prototype.log = isGM ? GM_log : (window.opera ? opera.postError : console.log);
+  GM_configStruct.prototype.log =  window.console ?
+    console.log : (isGM && typeof GM_log != 'undefined' ?
+      GM_log : (window.opera ?
+        opera.postError : function(){ /* no logging */ }
+  ));
 })();
 
 function GM_configDefaultValue(type, options) {
