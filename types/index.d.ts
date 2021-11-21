dde6fdc9c52a7c6a09be9436fd2d17151a3f5439
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-11-20 20:58:49 -0500

    Add LGPL comment to new files

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,29 @@
+/*
+Copyright 2009+, GM_config Contributors (https://github.com/sizzlemctwizzle/GM_config)
+
+GM_config Collaborators/Contributors:
+    Mike Medley <medleymind@gmail.com>
+    Joe Simmons
+    Izzy Soft
+    Marti Martz
+    Adam Thompson-Sharpe
+
+GM_config is distributed under the terms of the GNU Lesser General Public License.
+
+    GM_config is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // Minimum TypeScript Version: 2.8
 
 type FieldValue = string | number | boolean;
